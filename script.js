70e1f33b862feb6740b865a8308bd8fc53eb3bd5
alisonstardust <zakulisami@o2.pl>
2018-04-11 20:49:00 +0200

    what a mess

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let MyData = [
+let myData = [
   {
     "id": 1,
     "firstName": "Jan",
@@ -49,21 +49,94 @@ let MyData = [
   }
 ]
 
+const tableWebsite = document.querySelector('.table');
+
+let table = `<table><thead><tr><th>ID</th><th>First Name</th><th>Last Name</th><th>DoB</th><th>Company</th><th>Note</th></tr></thead>`;
+  table += '<tbody>';
+  for (var i in myData) {
+    table += `<tr>
+    <td>${myData[i].id}</td>
+    <td>${myData[i].firstName}</td>
+    <td>${myData[i].lastName}</td>
+    <td>${myData[i].dateOfBirth}</td>
+    <td>${myData[i].company}</td>
+    <td>${myData[i].note}</td>
+    </tr>`;
+}
+
+//innerHTML is the devil of devils, however right now it is the best solution I can come up with
+tableWebsite.innerHTML = table;
 
 
-//var mydata = JSON.parse(data);
-console.log(MyData)
-let names = MyData.map(name => console.log(name.firstName))
+// const first = document.querySelectorAll('.first td');
+// const second = document.querySelectorAll('.second td');
+// const third = document.querySelectorAll('.third td');
+// const fourth = document.querySelectorAll('.fourth td');
+// const fifth = document.querySelectorAll('.fifth td');
+// const sixth = document.querySelectorAll('.sixth td');
+// const button = document.querySelector('.sort-ID');
 
-/*
+// let ids = MyData.map(function(character) {
+//   return character.id;
+// });
 
-not sure if I am able to access the data from additional fila data.json, might be possible with local server and using ajax
-read more about this
+// let firstNames = MyData.map(function(character) {
+//   return character.firstName;
+// });
 
-$.ajax({
-  dataType: "json",
-  url: url,
-  data: data,
-  success: success
-});
-*/
+// let lastNames = MyData.map(function(character) {
+//   return character.lastName;
+// });
+
+// let datesOfBirth = MyData.map(function(character) {
+//   return character.dateOfBirth;
+// });
+
+// let companies = MyData.map(function(character) {
+//   return character.company;
+// });
+
+// let notes = MyData.map(function(character) {
+//   return character.note;
+// });
+
+// //sorting function
+// button.addEventListener("click", myFunction);
+
+// function myFunction (){
+//   ids.sort();
+//   // firstNames.sort();
+//   // lastNames.sort();
+//   // datesOfBirth.sort();
+//   // companies.sort();
+//   // notes.sort();
+//   // console.log(ids, firstNames)
+//    print()
+// }
+
+// function print() {
+//   for (i=0; i < ; i++) {
+//     first[i].innerText = ids[i];
+//     second[i].innerText = firstNames[i];
+//     third[i].innerText = lastNames[i];
+//     fourth[i].innerText = datesOfBirth[i];
+//     fifth[i].innerText = companies[i];
+//     sixth[i].innerText = notes[i];
+//   }
+// }
+
+// print()
+// /*
+
+// not sure if I am able to access the data from additional fila data.json, might be possible with local server and using ajax
+// read more about this
+
+// $.ajax({
+//   dataType: "json",
+//   url: url,
+//   data: data,
+//   success: success
+// });
+// //var mydata = JSON.parse(data);
+// //console.log(MyData)
+// */
